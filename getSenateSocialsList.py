d0ba109d1b70f6bed716364eb8b416840bf0614a
Antoine Washington <antoinewashington@Antoines-MacBook-Pro-2.local>
2025-11-28 21:49:06 -0500

    Getting a list of all Senate Dems names.

--- getSenateSocialsList.py
+++ getSenateSocialsList.py
@@ -3,3 +3,29 @@ Get A list of all Senate Democrats and their social media accounts.
 """
 
 import requests
+from bs4 import BeautifulSoup
+
+
+def get_dem_senate_caucus():
+    """Lookup a list of dictionaries of senate Democrat's  first
+    and last names so that we know who to retrieve socials for."""
+    response = requests.get(
+        "https://www.democrats.senate.gov/about-senate-dems/our-caucus"
+    )
+
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    senator_list = []
+    for senator in soup.find_all("div", class_="MemberBox__name"):
+        senator_name_list = [
+            name.get_text()
+            for name in senator.find_all("span")
+            if "Senator" not in name.get_text()
+        ]
+        senator_name_dict = {
+            "first_name": senator_name_list[0],
+            "last_name": senator_name_list[1],
+        }
+        senator_list.append(senator_name_dict)
+
+    return senator_list
