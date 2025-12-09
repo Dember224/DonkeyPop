1075259a14b097cbc598ba70fce50b402b6504bb
Antoine Washington <antoinewashington@Antoines-MacBook-Pro-2.local>
2025-12-09 00:28:16 -0500

    Making it so we only login once then re-use client. Get all senator method created.

--- bluesky.py
+++ bluesky.py
@@ -5,7 +5,10 @@ Get raw data on blue sky feeds for politicians.
 import os
 
 from atproto import Client
+from atproto_client.exceptions import BadRequestError
 from dotenv import load_dotenv
+
+from politician_lookup import get_dem_senate_caucus
 
 load_dotenv()  # This reads variables from the
 # .env file and loads them into os environment.
@@ -16,12 +19,21 @@ BLUESKY_HANDLE = os.getenv("BLUESKY_HANDLE")
 class BlueSkyData:
     def __init__(self):
         self.feed_results = []
+        self.failed_lookups = []
+        self.full_senate_data = []
+        self.client = None
 
     def login_bsky(self) -> Client:
         """Log the current user in to bluesky and return a client."""
-        client = Client()
-        client.login(BLUESKY_HANDLE, BLUESKY_PASSWORD)
-        return client
+        if self.client is None:
+            client = Client()
+            client.login(BLUESKY_HANDLE, BLUESKY_PASSWORD)
+            self.client = client
+            # This will allow us to log in once and avoid hitting
+            # bluesky's API limits of 100 logins per 5 minutes.
+            return client
+        else:
+            return self.client
 
     def parse_handles(self, last_name: str) -> str:
         """Take the last name of a candidate and return their bsky handle.
@@ -69,7 +81,26 @@ class BlueSkyData:
 
         return data.feed
 
+    def get_all_sentate_bsky(self) -> list[dict]:
+        for senator_dict in get_dem_senate_caucus():
+            full_name = (
+                f"{senator_dict["first_name"]} "
+                f"{senator_dict["last_name"]}"
+            )
+            print(f"getting {full_name}")
+            try:
+                senator_feed = self.lookup_feed(
+                    senator_dict["last_name"].lower().replace(" ", "")
+                )
+                this_senators_data = {f"{full_name}": senator_feed}
+                self.full_senate_data.append(this_senators_data)
+                self.feed_results.clear()
+            except BadRequestError as e:
+                print(f"Failed lookup for {full_name} due to: {e}")
+                self.failed_lookups.append(full_name)
+                continue
+        return self.full_senate_data
+
 
 bsky_data = BlueSkyData()
-bsky_data.lookup_feed(politician_last_name="alsobrooks")
-print(bsky_data.feed_results, "Place")
+print(bsky_data.get_all_sentate_bsky())
